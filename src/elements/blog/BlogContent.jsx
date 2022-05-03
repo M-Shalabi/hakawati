c94c5a9d5d174b629704ed78b15bbf7149b8ec97
Muyassr <moyasr.tam@gmail.com>
2022-05-03 14:47:16 +0300

    first commit

---
+++ src/elements/blog/BlogContent.jsx
@@ -0,0 +1,25 @@
+const BlogContent = [
+    {
+        images: '01',
+        title: 'Leap Event',
+        category: 'Development'
+    },
+    {
+        images: '02',
+        title: 'Finalist on QXl in Austuralia',
+        category: 'Management'
+    },
+
+    {
+        images: '03',
+        title: 'Won Ithra and demo in London',
+        category: 'Design'
+    },
+    {
+        images: '04',
+        title: 'Workshop at Almadinah developmen',
+        category: 'Development'
+    },
+]
+
+export default BlogContent;
