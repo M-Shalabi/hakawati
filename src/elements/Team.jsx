1fd0e0f016baf4b912c50f54946efc6faa0e776c
Muyassr <moyasr.tam@gmail.com>
2022-05-08 01:58:40 +0300

    second commit

--- src/elements/Team.jsx
+++ src/elements/Team.jsx
@@ -1,141 +1,164 @@
 import React, { Component } from "react";
-import { FaFacebookF , FaLinkedinIn , FaTwitter } from "react-icons/fa";
+import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 let TeamContent = [
-    {
-        images: '01',
-        title: 'Jone Due',
-        designation: 'Sr. Web Developer',
-        socialNetwork: [
-            {
-                icon: <FaFacebookF />,
-                url: '#'
-            },
-            {
-                icon: <FaLinkedinIn />,
-                url: '#'
-            },
-            {
-                icon: <FaTwitter />,
-                url: '#'
-            },
-        ]
-    },
-    {
-        images: '02',
-        title: 'Fatima Asrafi',
-        designation: 'Front-end Engineer',
-        socialNetwork: [
-            {
-                icon: <FaFacebookF />,
-                url: '#'
-            },
-            {
-                icon: <FaLinkedinIn />,
-                url: '#'
-            },
-        ]
-    },
-    {
-        images: '03',
-        title: 'Al-Amin Bali',
-        designation: 'Sr. Graphic Designer',
-        socialNetwork: [
-            {
-                icon: <FaFacebookF />,
-                url: '#'
-            },
-            {
-                icon: <FaLinkedinIn />,
-                url: '#'
-            },
-            {
-                icon: <FaTwitter />,
-                url: '#'
-            },
-        ]
-    },
-    {
-        images: '06',
-        title: 'Jone Due',
-        designation: 'Sr. Web Developer',
-        socialNetwork: [
-            {
-                icon: <FaFacebookF />,
-                url: '#'
-            },
-            {
-                icon: <FaLinkedinIn />,
-                url: '#'
-            },
-            {
-                icon: <FaTwitter />,
-                url: '#'
-            },
-        ]
-    },
-    {
-        images: '05',
-        title: 'Fatima Asrafi',
-        designation: 'Front-end Engineer',
-        socialNetwork: [
-            {
-                icon: <FaFacebookF />,
-                url: '#'
-            },
-            {
-                icon: <FaLinkedinIn />,
-                url: '#'
-            },
-        ]
-    },
-    {
-        images: '12',
-        title: 'Al-Amin Bali',
-        designation: 'Sr. Graphic Designer',
-        socialNetwork: [
-            {
-                icon: <FaFacebookF />,
-                url: '#'
-            },
-            {
-                icon: <FaLinkedinIn />,
-                url: '#'
-            },
-            {
-                icon: <FaTwitter />,
-                url: '#'
-            },
-        ]
-    }
+  {
+    images: "01",
+    title: "Abdullah Ba Mashmous",
+    designation: "CEO & Producer",
+    socialNetwork: [
+      {
+        icon: <FaFacebookF />,
+        url: "#",
+      },
+      {
+        icon: <FaLinkedinIn />,
+        url: "#",
+      },
+      {
+        icon: <FaTwitter />,
+        url: "#",
+      },
+    ],
+  },
+  {
+    images: "02",
+    title: "Molham AlEter",
+    designation: "Art Director",
+    socialNetwork: [
+      {
+        icon: <FaFacebookF />,
+        url: "#",
+      },
+      {
+        icon: <FaLinkedinIn />,
+        url: "#",
+      },
+    ],
+  },
+  {
+    images: "03",
+    title: "Mohammed Shalabi",
+    designation: "Developer",
+    socialNetwork: [
+      {
+        icon: <FaFacebookF />,
+        url: "#",
+      },
+      {
+        icon: <FaLinkedinIn />,
+        url: "#",
+      },
+      {
+        icon: <FaTwitter />,
+        url: "#",
+      },
+    ],
+  },
+  {
+    images: "04",
+    title: "Moyasr Tamim",
+    designation: "UX/UI Designer",
+    socialNetwork: [
+      {
+        icon: <FaFacebookF />,
+        url: "#",
+      },
+      {
+        icon: <FaLinkedinIn />,
+        url: "#",
+      },
+      {
+        icon: <FaTwitter />,
+        url: "#",
+      },
+    ],
+  },
+  {
+    images: "05",
+    title: "Roua Alzahem",
+    designation: "Visual Artist",
+    socialNetwork: [
+      {
+        icon: <FaFacebookF />,
+        url: "#",
+      },
+      {
+        icon: <FaLinkedinIn />,
+        url: "#",
+      },
+    ],
+  },
+  {
+    images: "06",
+    title: "Omar Balfaqaih",
+    designation: "Game Developer",
+    socialNetwork: [
+      {
+        icon: <FaFacebookF />,
+        url: "#",
+      },
+      {
+        icon: <FaLinkedinIn />,
+        url: "#",
+      },
+      {
+        icon: <FaTwitter />,
+        url: "#",
+      },
+    ],
+  },
+  {
+    images: "07",
+    title: "Atheer Minwar",
+    designation: "Sound/Level Designer",
+    socialNetwork: [
+      {
+        icon: <FaFacebookF />,
+        url: "#",
+      },
+      {
+        icon: <FaLinkedinIn />,
+        url: "#",
+      },
+      {
+        icon: <FaTwitter />,
+        url: "#",
+      },
+    ],
+  },
 ];
 
-
-class Team extends Component{
-    render(){
-        const {column} = this.props;
-        return(
-            <React.Fragment>
-                {TeamContent.map((value , i ) => (
-                    <div className={`${column}`} key={i}>
-                        <div className="team">
-                            <div className="thumbnail">
-                                <img src={`/assets/images/team/team-${value.images}.jpg`} alt="Blog Images"/>
-                            </div>
-                            <div className="content">
-                                <h4 className="title">{value.title}</h4>
-                                <p className="designation">{value.designation}</p>
-                            </div>
-                            <ul className="social-icon" >
-                                {value.socialNetwork.map((social, index) =>
-                                    <li key={index}><a href={`${social.url}`}>{social.icon}</a></li>
-                                )}
-                            </ul>
-                        </div>
-                    </div>
+class Team extends Component {
+  render() {
+    const { column } = this.props;
+    return (
+      <React.Fragment>
+        {TeamContent.map((value, i) => (
+          <div className={`${column}`} key={i}>
+            <div className="team">
+              <div className="thumbnail">
+                <img
+                  src={`/assets/images/team/team-${value.images}.jpg`}
+                  alt="Blog Images"
+                />
+              </div>
+              <div className="content">
+                <h4 className="title">{value.title}</h4>
+                <p className="designation">{value.designation}</p>
+              </div>
+              <ul className="social-icon">
+                {value.socialNetwork.map((social, index) => (
+                  <li key={index}>
+                    <a href={`${social.url}`}>{social.icon}</a>
+                  </li>
                 ))}
-            </React.Fragment>
-        )
-    }
+              </ul>
+            </div>
+          </div>
+        ))}
+      </React.Fragment>
+    );
+  }
 }
 export default Team;
